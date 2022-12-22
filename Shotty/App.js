0b98f21be7ae39b46144559ed0c09a04930d6b81
JeliHacker <jeligooch@gmail.com>
2022-12-22 00:12:09 -0800

    add hole functionality

--- Shotty/App.js
+++ Shotty/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, TouchableOpacity, Pressable } from 'react-native';
 import React, { useState, useEffect } from 'react';
 
 
@@ -10,46 +10,27 @@ export default function App() {
   var image2Source = require('../Shotty/assets/white-claw-with-hole.png')
 
   const [source, setSource] = useState(imageSource);
+  const [imagingSource, setImageSource] = useState(imageSource);
 
-  const [isPressed, setIsPressed] = useState(false);
-
-  const handlePress = () => {
-    setIsPressed(true);
-    setTimeout(() => {
-      console.log('Button pressed 5 seconds ago');
-    }, 5000);
-  };
-
-  const [pressTimer, setPressTimer] = useState(null);
-
-
-  useEffect(() => {
-    if (isPressed) {
-      setPressTimer(setTimeout(() => {
-        console.log('Button held for 5 seconds');
-      }, 5000));
-    } else {
-      clearTimeout(pressTimer);
-    }
-  }, [isPressed, pressTimer]);
+  const putHole = () => {
+    setSource(imageSource === imageSource ? image2Source : imageSource);
+  }
 
   return (
     <View style={styles.container}>
 
       <Image
-        source={source}
+        source={imagingSource}
         style={{ height: "95%", width: "95%" }}
       />
 
       <TouchableOpacity
         style={styles.touchableStyle}
-        // onPress={() => {
-        //   console.log("Pressed!")
-        //   handlePress()
-        //   setSource(imageSource)
-        // }}
-        onPressIn={() => { setIsPressed(true) }}
-        onPressOut={() => { setIsPressed(false) }}
+        onLongPress={() => {
+          putHole()
+          setImageSource(imagingSource === imageSource ? image2Source : imageSource);
+        }}
+        delayLongPress={800}
       />
       <StatusBar style="auto" />
     </View>
